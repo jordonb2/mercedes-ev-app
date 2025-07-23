5965b28210210aef1cff7c0518c035a721a4e642
jordonb2 <jordonbwn3@gmail.com>
2025-07-23 15:19:34 -0400

    Initial Commit
    Folders created, added ChargingStation model for OpenChargeMap response

---
+++ MercedesEV/MercedesEV/MercedesEVApp.swift
@@ -0,0 +1,17 @@
+//
+//  MercedesEVApp.swift
+//  MercedesEV
+//
+//  Created by Jordon Bowen on 7/23/25.
+//
+
+import SwiftUI
+
+@main
+struct MercedesEVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MercedesEV/MercedesEV/Models/ChargingStation.swift
@@ -0,0 +1,37 @@
+//
+//  ChargingStation.swift
+//  MercedesEV
+//
+//  Created by Jordon Bowen on 7/23/25.
+//
+
+import Foundation
+
+struct ChargingStation: Decodable, Identifiable {
+    let id: Int
+    let addressInfo: AddressInfo
+    let connections: [Connection]?
+    let usageType: UsageType?
+
+    struct AddressInfo: Decodable {
+        let title: String?
+        let addressLine1: String?
+        let town: String?
+        let stateOrProvince: String?
+        let postcode: String?
+        let latitude: Double?
+        let longitude: Double?
+    }
+
+    struct Connection: Decodable {
+        let connectionType: ConnectionType?
+
+        struct ConnectionType: Decodable {
+            let title: String?
+        }
+    }
+
+    struct UsageType: Decodable {
+        let title: String?
+    }
+}
